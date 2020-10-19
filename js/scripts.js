48ead38f56981f2fc2da38f488b5c97252428d03
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-10-19 10:10:25 -0700

    clarify functions

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,8 @@ function add(number1, number2) {
   
   const number1 = parseInt(prompt("Enter a number:"));
   const number2 = parseInt(prompt("Enter another Number:"));
-  alert(add(number1, number2));
+  const result = add(number1, number2);
+  alert(result);
   
   
   
