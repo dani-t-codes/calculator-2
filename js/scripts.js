5332c8a3bb96dbde8befa4d3d737ffadb5a4d838
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-10-21 14:23:37 -0700

    clean up and expand script capabilities, begin deliniation between business/user logic
    Co-authored-by: Danielle Thompson <71739178+dani-t-codes@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,43 @@
-function multiply(number1, number2) {
-  return number1 * number2;
-  }
-  
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another Number:"));
-  const result = multiply(number1, number2);
-  alert(result);
-  
-  
-  
-  
+// Start of business logic
+const add = function(num1, num2) {
+  return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+  return num1 - num2;
+};
+
+const multiply = function(num1, num2) {
+  return num1 - num2;
+};
+
+const divide = function(num1, num2) {
+  return num1 / num2;
+};
+// End of business logic
+
+// Start of user interface logic
+
+const num1 = parseInt(prompt("Enter a number:"));
+const num2 = parseInt(prompt("Enter another number:"));
+alert(add(num1, num2));
+
+
+
+
+// End of user interface logic
+
+// Start of deleted code
+
+// function multiply(number1, number2) {
+//   return number1 * number2;
+//   }
+
+//   const number1 = parseInt(prompt("Enter a number:"));
+//   const number2 = parseInt(prompt("Enter another Number:"));
+//   const result = multiply(number1, number2);
+//   alert(result);
+
+
+
+// end of code end of page END!!! END!!!!!!!
