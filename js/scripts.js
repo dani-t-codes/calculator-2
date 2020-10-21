6ee1ec1cc41a35acffac6b89fc2be75c2dc4f282
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-10-21 15:42:42 -0700

    collect value from 'add1' and 'add2' input fields
    Co-authored-by: Danielle Thompson <71739178+dani-t-codes@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,19 +14,42 @@ const multiply = function(num1, num2) {
 const divide = function(num1, num2) {
   return num1 / num2;
 };
-// End of business logic
+// End of business lgic
 
 // Start of user interface logic
 $(document).ready(function() {
-  $("#add").submit(function(event) {
-    event.preventDefault();
-  });
-  const num1 = parseInt(prompt("Enter a number:"));
-  const num2 = parseInt(prompt("Enter another number:"));
-  
+  const num1 = parseInt($("#add1").val());
+  const num2 = parseInt($("#add2").val());
   alert(add(num1, num2));
 });
 
+
+
+
+
+
+
+// $(document).ready(function() {
+  // $("form#add").submit(function(event) {
+  //   event.preventDefault();
+  });
+  const num1 = parseInt(text("Enter a number:"));
+  const num2 = parseInt(text("Enter another number:"));
+  const num3 = parseInt(a("num1 + num2"))
+
+  //$("").text
+  
+  //// show(add(num1, num2));
+});
+
+$(document).ready(function() {
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+  });
+  //const num1 = parseInt(prompt("Enter a number:"));
+  //const num2 = parseInt(prompt("Enter another number:"));
+  
+  //show(add(num1, num2));
 
 
 
