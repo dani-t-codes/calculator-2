3c436e5a52fb6a80850f840e0aa18c74a22f7cc0
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-10-21 16:02:57 -0700

    correct error on line 21
    Co-authored-by: Danielle Thompson <71739178+dani-t-codes@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -18,10 +18,11 @@ const divide = function(num1, num2) {
 
 // Start of user interface logic
 $(document).ready(function() {
-  $(#form.add").submit(function(event) {}
+  $(#form.add").submit(function(event) {
   const num1 = parseInt($("#add1").val());
   const num2 = parseInt($("#add2").val());
   alert(add(num1, num2));
+  });
 });
 
 
